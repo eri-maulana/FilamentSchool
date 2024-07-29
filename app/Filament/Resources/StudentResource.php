53831e61eda3789e130f7b74656160735889a858
eri-maulana <maulanabouble11@gmail.com>
2024-07-29 09:21:57 +0700

    13. Modal Form

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -9,12 +9,14 @@ use App\Models\Student;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Actions\BulkAction;
-use Illuminate\Database\Eloquent\Collection;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use App\Filament\Resources\StudentResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\StudentResource\RelationManagers;
@@ -76,17 +78,25 @@ class StudentResource extends Resource
                     }
                 ),
                 Tables\Columns\TextColumn::make('nis')
+                ->label('NIS')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
+                ->label('Nama Murid')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('gender'),
+            Tables\Columns\TextColumn::make('gender')
+                ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('birthday')
-                    ->date()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('religion'),
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('religion')
+                ->label('Agama'),
                 Tables\Columns\TextColumn::make('contact')
-                    ->searchable(),
-                ImageColumn::make('profile'),
+                ->label('Kontak'),
+            ImageColumn::make('profile')
+                ->label('Profil'),
+            TextColumn::make('status')
+                ->label('Status')
+                ->toggleable(isToggledHiddenByDefault: true)
+                ->formatStateUsing(fn (string $state): string => ucwords("{$state}")),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -94,31 +104,35 @@ class StudentResource extends Resource
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            TextColumn::make('status')
-            ->formatStateUsing(fn (string $state): string => ucwords("{$state}"))
+                ->toggleable(isToggledHiddenByDefault: true),
             ])
-            ->filters([
-                //
+            ->filters([SelectFilter::make('status')
+                ->multiple()
+                ->options([
+                    'accept' => 'Accept',
+                    'off' => 'Off',
+                    'move' => 'Move',
+                    'grade' => 'Grade',
+                ])
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
-            ->bulkActions([Tables\Actions\BulkActionGroup::make([
-                BulkAction::make('Accept')
-                    ->icon('heroicon-m-check')
-                    ->requiresConfirmation()
-                    ->action(function (Collection $records) {
-                        return $records->each->update(['status' => 'accept']);
-                    }),
-                BulkAction::make('Off')
-                    ->icon('heroicon-m-x-circle')
-                    ->requiresConfirmation()
-                    ->action(function (Collection $records) {
-                        return $records->each(function ($record) {
-                            $id = $record->id;
-                            Student::where('id', $id)->update(['status' => 'off']);
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    BulkAction::make('Ubah Status')
+                        ->icon('heroicon-c-arrow-path')
+                        ->requiresConfirmation()
+                    ->form([
+                        Select::make('Status')
+                            ->label('Status')
+                            ->options(['accept' => 'Accept', 'off' => 'Off', 'move' => 'Move', 'grade' => 'Grade',])
+                            ->required(),
+                    ])
+                    ->action(function (Collection $records, array $data) {
+                        $records->each(function ($record) use ($data) {
+                            Student::where('id', $record->id)->update(['status' => $data['Status']]);
                         });
                     }),
                     Tables\Actions\DeleteBulkAction::make(),
