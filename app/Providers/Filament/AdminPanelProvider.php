227514f68263f365824e777f8b6b3229a8949e64
eri-maulana <maulanabouble11@gmail.com>
2024-08-02 10:16:46 +0700

    18. Role & Permission

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,20 +6,35 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
 use Filament\Facades\Filament;
 use Filament\Support\Colors\Color;
 use Filament\Navigation\UserMenuItem;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\NavigationGroup;
 use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Navigation\NavigationBuilder;
 use App\Filament\Resources\PeriodeResource;
+use App\Filament\Resources\StudentResource;
+use App\Filament\Resources\SubjectResource;
+use App\Filament\Resources\TeacherResource;
+use App\Filament\Resources\ClassroomResource;
+use App\Filament\Resources\DepartmentResource;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use App\Filament\Resources\CategoryNilaiResource;
+use App\Filament\Resources\StudentHasClassResource;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+use App\Filament\Pages\Settings;
+use App\Filament\Resources\UserResource;
+
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -62,7 +77,55 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->viteTheme('resources/css/filament/admin/theme.css');
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make('')
+                        ->items([
+                            NavigationItem::make('Dashboard')
+                            ->icon('heroicon-o-home')
+                            ->isActiveWhen(fn (): bool => request()->routeIs('filament.admin.pages.dashboard'))
+                            ->url(fn (): string => Dashboard::getUrl()),
+                        ]),
+                    NavigationGroup::make('Academic')
+                    ->items([
+                        ...TeacherResource::getNavigationItems(),
+                        ...StudentResource::getNavigationItems(),
+                        ...StudentHasClassResource::getNavigationItems(),
+                        ...SubjectResource::getNavigationItems(),
+                    ]),
+                    NavigationGroup::make('Source')
+                        ->items([
+                            ...CategoryNilaiResource::getNavigationItems(),
+                            ...ClassroomResource::getNavigationItems(),
+                            ...DepartmentResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Setting')
+                    ->items([
+                        ...PeriodeResource::getNavigationItems(),
+
+                        NavigationItem::make('Roles')
+                            ->icon('heroicon-o-lock-closed')
+                            ->isActiveWhen(fn (): bool => request()->routeIs(
+                                'filament.admin.resources.roles.index',
+                                'filament.admin.resources.roles.create',
+                                'filament.admin.resources.roles.view',
+                                'filament.admin.resources.roles.edit',
+                            ))
+                            ->url(fn (): string => '/admin/roles'),
+                        NavigationItem::make('Permission')
+                        ->icon('heroicon-o-user-group')
+                        ->isActiveWhen(fn (): bool => request()->routeIs(
+                            'filament.admin.resources.permissions.index',
+                            'filament.admin.resources.permissions.create',
+                            'filament.admin.resources.permissions.view',
+                            'filament.admin.resources.permissions.edit',
+                        ))
+                            ->url(fn (): string => '/admin/permissions'),
+                    ]),
+                ]);
+            });
     }
 
     public function boot(): void
